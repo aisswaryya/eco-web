a1aa70d35d3d1879ac520712f74aacaa9f593af2
Thushanth Bengre <bengre.t@husky.neu.edu>
2019-12-02 17:21:37 -0500

    bengre-frontend: Added jest

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FundraiserListComponent } from './fundraiser-list/fundraiser-list.component';
+import { FundraiserDetailComponent } from './fundraiser-detail/fundraiser-detail.component';
+import { MyFundraiserListComponent } from './my-fundraiser-list/my-fundraiser-list.component';
+import { MyFundraiserDetailComponent } from './my-fundraiser-detail/my-fundraiser-detail.component';
+import { FundraiserCreateComponent } from './fundraiser-create/fundraiser-create.component';
+import { DonationCreateComponent } from './donation-create/donation-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    FundraiserListComponent,
+    FundraiserDetailComponent,
+    MyFundraiserListComponent,
+    MyFundraiserDetailComponent,
+    FundraiserCreateComponent,
+    DonationCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/my-fundraiser-detail/my-fundraiser-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-fundraiser-detail',
+  templateUrl: './my-fundraiser-detail.component.html',
+  styleUrls: ['./my-fundraiser-detail.component.scss']
+})
+export class MyFundraiserDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
