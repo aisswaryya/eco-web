5419dd44442f1f11f6481422a72b13d18d7d61a7
Aisswaryya <murugananth.a@husky.neu.edu>
2019-12-02 23:48:15 -0500

    Added Petition Module

---
+++ frontend/src/app/petition/petition.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { SharedModule} from '../shared/shared.module';
+import {CreatePetitionComponent} from './create-petition/create-petition.component';
+import { ViewPetitionComponent} from './view-petition/view-petition.component';
+import { ViewByIDPetitionComponent} from './view-by-id-petition/view-by-id-petition.component';
+import { UpdatePetitionComponent} from './update-petition/update-petition.component';
+import { CommonModule } from '@angular/common';
+import { PetitionRoutingModule } from './petition-routing/petition-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations:[CreatePetitionComponent, 
+    ViewPetitionComponent,
+    ViewByIDPetitionComponent,
+    UpdatePetitionComponent],
+    
+  imports: [
+    CommonModule,
+    PetitionRoutingModule,
+    ReactiveFormsModule,
+    SharedModule
+  ]  
+})
+export class PetitionModule { 
+
+
+}
