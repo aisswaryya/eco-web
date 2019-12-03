026871a3efa5a8caf8a7c8f9811c00ccc902a045
Aisswaryya <murugananth.a@husky.neu.edu>
2019-12-02 23:50:29 -0500

    Added View Petition

---
+++ frontend/src/app/petition/view-petition/view-petition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-petition',
+  templateUrl: './view-petition.component.html',
+  styleUrls: ['./view-petition.component.scss']
+})
+export class ViewPetitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
