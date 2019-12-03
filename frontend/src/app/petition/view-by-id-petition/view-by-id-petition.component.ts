abeb5be69ed9465f55e2b6c0179f0f0ab4b81d07
Aisswaryya <murugananth.a@husky.neu.edu>
2019-12-02 23:58:07 -0500

    Added ViewByID Petition

---
+++ frontend/src/app/petition/view-by-id-petition/view-by-id-petition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-by-id-petition',
+  templateUrl: './view-by-id-petition.component.html',
+  styleUrls: ['./view-by-id-petition.component.scss']
+})
+export class ViewByIDPetitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
