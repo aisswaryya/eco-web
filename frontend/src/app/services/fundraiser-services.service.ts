03ab906f4244933b1b00f7f444d55f5adbe350e4
Thushanth Bengre <bengre.t@husky.neu.edu>
2019-12-03 13:42:27 -0500

    bengre-frontend: added services

--- frontend/src/app/services/fundraiser-services.service.ts
+++ frontend/src/app/services/fundraiser-services.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs';
+import {Fundraiser} from '../models/fundraiser';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,38 @@ export class FundraiserServicesService {
     this.fundraiserResourceUrl = `${environment.serverBaseURL}/${this.baseUrl}/${this.fundraiserResource}`;
   }
 
-getFundraisers(): Observable<Array<Fun>>
+  getFundraisers(): Observable<Array<Fundraiser>> {
+    return this.http.get<Array<Fundraiser>>(this.fundraiserResourceUrl);
+  }
+
+  getFundraisersByEmailId(emailId: string): Observable<Array<Fundraiser>>{
+    return this.http.get<Array<Fundraiser>>(`${this.fundraiserResourceUrl}?emailId=${emailId}`);
+  }
+
+  getFundraiser(id: string): Observable<Fundraiser> {
+    return this.http.get<Fundraiser>(`${this.fundraiserResourceUrl}/${id}`);
+  }
+
+  createFundraiser(fundraiser: Fundraiser = null): Observable<Fundraiser> {
+    let newFundraiser: Fundraiser;
+    newFundraiser = fundraiser ? fundraiser : new Fundraiser(
+        'Untitled Fundraiser',
+        'NONE',
+        false,
+        '',
+        '',
+        '',
+        0,
+        0);
+    return this.http.post<Fundraiser>(this.fundraiserResourceUrl, newFundraiser);
+  }
+
+  updateFundraiser(id: string, data: Fundraiser): Observable<Fundraiser> {
+    return this.http.put<Fundraiser>(`${this.fundraiserResourceUrl}/${id}`, data);
+  }
+
+  deleteFundraiser(id: string) {
+    return this.http.delete(`${this.fundraiserResourceUrl}/${id}`);
+  }
+
 }
