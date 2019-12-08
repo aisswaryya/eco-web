08ab158f40eca8089835c27a19996e8b5c6a54cc
Aisswaryya <murugananth.a@husky.neu.edu>
2019-12-08 16:16:58 -0500

    updated route

--- backend/route/eco.route.js
+++ backend/route/eco.route.js
@@ -10,13 +10,15 @@ module.exports = (app) => {
 
     // Retrieve a single petition with petitionId
     app.get('/petition/:petitionId', petition.findOne);
-
     
     // Update a Petition with petitionId
     app.put('/petition/:petitionId', petition.update);
 
     // Delete a Petition with petitionId
     app.delete('/petition/:petitionId', petition.delete);
+
+    //Retrieve petition by emailId
+    app.get('/petition/email/:email',petition.findByEmailId);
 
     // Create a new Signature
     app.post('/signature', signature.create);
@@ -32,12 +34,9 @@ module.exports = (app) => {
 
     // Retrieve a signature count with petitionId
     app.get('/signature/count/:petitionId', signature.getSignatureCount);
+ 
+    // Retrieve a signature by emailId
+    app.get('/signature/email/:email', signature.findByEmailId);
 
-    // Update a signature with petitionId
-    app.put('/signature/:petitionId', signature.update);
-
-    // Delete a Signature with petitionId
-    app.delete('/signature/:petitionId', signature.delete);
-    
-    
+ 
 }
