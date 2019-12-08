bd07d3ba53bc82dff06b0f6c345fd4b30bfa224d
Thushanth Bengre <bengre.t@husky.neu.edu>
2019-12-08 12:24:04 -0500

    bengre-frontend: Added create fundraiser component

--- frontend/src/app/fundraiser-create/fundraiser-create.component.ts
+++ frontend/src/app/fundraiser-create/fundraiser-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Fundraiser} from '../models/fundraiser';
+import {FundraiserServicesService} from '../services/fundraiser-services.service';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-fundraiser-create',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FundraiserCreateComponent implements OnInit {
 
-  constructor() { }
+    fundraiserCategory = ['Birthday Fundraisers',
+        'Team Campaign',
+        'Community Garden',
+        'Composting Program',
+        'Earth Day Fundraiser',
+        'Other']
+
+  fundraiser = new Fundraiser('',
+      '',
+      true,
+      '',
+      '',
+      '',
+      '',
+      '',
+      0,
+      0);
+
+  constructor(private fundraiserService: FundraiserServicesService,
+              private router: Router) { }
 
   ngOnInit() {
   }
+
+
+  saveFundraiser(form: NgForm) {
+      form.value.emailId = 'thushu22@gmail.com';
+      this.fundraiserService.createFundraiser(form.value).subscribe(data => {
+          alert('Fundraiser Created');
+          console.log(data);
+          this.router.navigate(['/fundraiser-detail', data.id]);
+      }, error => {
+          console.log(error);
+      });
+  }
+
+
 
 }
